8e8b4464aba01ed0957d6b28d2230634e3ce1999
GuiHaiTao <494887135@qq.com>
2023-04-21 17:40:44 +0800

    fix(<package config>): <add cjs>

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  esm: {
+    output: 'es',
+    platform: 'browser', // 默认构建为 Browser 环境的产物
+    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+  },
+  cjs: {
+    output: 'dist',
+    platform: 'node', // 默认构建为 Node.js 环境的产物
+    transformer: 'esbuild', // 默认使用 esbuild 以获得更快的构建速度
+  },
 });
